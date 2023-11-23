133b759590257d38992599ab46565432f5a93db9
JaeHyun Lee <104538469+JaeHyunLee123@users.noreply.github.com>
2023-11-23 15:35:47 +0900

    feat: add menu bar

--- panoramaMFC/Resource.h
+++ panoramaMFC/Resource.h
@@ -7,6 +7,7 @@
 #define IDS_ABOUTBOX                    101
 #define IDD_PANORAMAMFC_DIALOG          102
 #define IDR_MAINFRAME                   128
+#define IDR_MENU1                       130
 #define IDC_BUTTON_LEFT_IMAGE           1001
 #define IDC_STATIC_LEFT_IMAGE           1002
 #define IDC_BUTTON_RIGHT_IMAGE          1003
@@ -16,13 +17,18 @@
 #define IDC_STATIC_PANORAMA_IMAGE       1007
 #define IDC_BUTTON_STITCH               1008
 #define IDC_BUTTON_SAVE                 1009
+#define ID_FILE_OPENLEFTIMAGE           32771
+#define ID_FILEOPEN_LEFTIMAGE           32772
+#define ID_FILEOPEN_CENTERIMAGE         32773
+#define ID_FILEOPEN_RIGHTIMAGE          32774
+#define ID_FILE_FILESAVE                32775
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        130
-#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32776
 #define _APS_NEXT_CONTROL_VALUE         1007
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
