0f50a844cff221bbb6e15df5bbc830bcfdc8e9bf
JaeHyun Lee <104538469+JaeHyunLee123@users.noreply.github.com>
2023-11-26 21:05:06 +0900

    refactor: merge all algorithm code to panaromaMFCDlg file

--- panoramaMFC/panoramaMFCDlg.h
+++ panoramaMFC/panoramaMFCDlg.h
@@ -5,12 +5,29 @@
 #pragma once
 
 #include<opencv2/opencv.hpp>
+#include<vector>
+
+using namespace cv;
+using namespace std;
 
 enum IMAGE_TYPE {
 	LEFT,
 	CENTER,
 	RIGHT,
 	PANORAMA
+};
+
+struct RGB {
+	int red;
+	int green;
+	int blue;
+};
+
+struct HSI
+{
+	int hue; // 0 ~ 180/PI 사이 값
+	int saturation; //0 ~ 100
+	int intensity; // 0 ~ 255
 };
 
 // CpanoramaMFCDlg 대화 상자
@@ -41,15 +58,24 @@ protected:
 	afx_msg void OnPaint();
 	afx_msg HCURSOR OnQueryDragIcon();
 	DECLARE_MESSAGE_MAP();
+
+
+	//stitching 알고리즘 함수
+	Mat blendImage(Mat image, vector<int> center, int blendingArea, int errorRange);
+	vector<RGB> blendRow(vector<RGB> input, int center, int blendingArea, int errorRange);
+	RGB getRGB(Mat image, int col, int row);
+	void putRGB(Mat& dest, RGB rgb, int col, int row);
+	RGB hsi2rgb(HSI input);
+	HSI rgb2hsi(RGB input);
+	Mat stitch_two_image(Mat original_image, Mat object_image);
+	bool compareX(const Point& p1, const Point& p2);
 	
 public:
 	CStatic LeftControl, CenterControl, RightControl, PanoramaControl;
 	CRect rect;
 	CDC* m_pDC;
 
-
 	afx_msg void OnBnClickedButtonStitch();
-
 	afx_msg void OnFileopenLeftImage();
 	afx_msg void OnFileopenCenterImage();
 	afx_msg void OnFileopenRightImage();
